5f2d8d63fcac3be1b5200846506c1103afc92c64
unknown <anasser@mazeed.co>
2022-12-09 03:07:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from 'react';
+import { PDFExport, savePDF } from '@progress/kendo-react-pdf';
+import googleimg from "./download.png"
 
 function App() {
+
+  const pdfExportComponent = useRef(null)
+  const contentArea = useRef(null)
+
+  const handleClickWithComponent = (e) => {
+    console.log("log");
+    pdfExportComponent.current.save();
+  }
+
+  const handleClickWithMethod = (e) => {
+    savePDF(contentArea.current, {paperSize: "A4"});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center items-center mt-5">
+      <PDFExport ref={pdfExportComponent} paperSize="A4">
+      <div ref={contentArea}>
+        <div>
+          <img src={googleimg} alt="img" />
+          <p>this is test app</p>
+          <h1>this is test app</h1>
+          <p>احمد ناصر حسن</p>
+        </div>
+      </div>
+      </PDFExport>
+      <button className='bg-gray-400 py-2 px-4' onClick={handleClickWithComponent}>save component</button>
+      <button className='bg-gray-400 py-2 px-4' onClick={handleClickWithMethod}>save method</button>
     </div>
   );
 }
